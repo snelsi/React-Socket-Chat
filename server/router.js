f9e3758ad173142e2cb1a907254af802fe04a73e
Roman Zhuravlov <SnelsiThePirate@gmail.com>
2020-02-20 00:39:46 +0200

    Server side update

--- server/router.js
+++ server/router.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, res) => {
+router.get("/", (_, res) => {
   res.send({ response: "Server is up and running." }).status(200);
 });
 
